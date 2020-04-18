9241863e08bd1b6a4c0f87c5bad890e295a34629
Stephen Dotz <2078064+sdotz@users.noreply.github.com>
2020-04-17 21:17:34 -0400

    make some improvements to style

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,19 +5,12 @@ results=$(java -jar /checkstyle.jar $* | awk NF | grep WARN)
 
 if [ -z "$results" ]; then
   message="code style looks good"
-  if [ -n "$GITHUB_ACTIONS" ]; then
-    echo "::set-output name=results::$message"
-  else
-    echo $message
-  fi
-  exit 0
 else
   message=$results
-  if [ -n "$GITHUB_ACTIONS" ]; then
-    echo "::set-output name=results::$message"
-  else
-    echo $results
-  fi
-  exit 1
 fi
 
+if [ -n "$GITHUB_ACTIONS" ]; then
+  echo "::set-output name=results::$message"
+else
+  echo $message
+fi
